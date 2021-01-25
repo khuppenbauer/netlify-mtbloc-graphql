3b1d092149e502a3fb9ee8b217f3e8e354caabd8
Kerstin Huppenbauer <k.huppenbauer@web.de>
2021-01-25 07:44:08 +0100

    TASK: Optimize GraphQL
    Add Track Query
    Add Json type
    Add GeoJson Schema
    Only query requested fields in allTracks

--- functions/graphql/resolvers.js
+++ functions/graphql/resolvers.js
@@ -1,13 +1,25 @@
+const GraphQLJSON = require('graphql-type-json');
+const ObjectId = require('mongodb').ObjectId;
+const GraphQLFields = require('graphql-fields');
+
 module.exports = db => ({
+  JSON: GraphQLJSON,
   Query: {
-    allTracks: async () => {
-      const tracks = await db.collection('tracks').find().toArray()
-      return tracks.map (track => {
-        return {
-          ...track,
-          id: track._id,
-        };
-      });
-    }
+    allTracks: async (parent, args, context, info) => {
+      const fields = Object.keys(GraphQLFields(info));
+      const projection = fields.reduce((a, b) => ({ ...a, [b]: 1 }), {})
+      return await db.collection('tracks').find({}, { projection }).toArray();
+    },
+    track: async (parent, args, context, info) => {
+      const track = await db.collection('tracks').findOne({_id: new ObjectId(args._id)});
+      return {
+        ...track,
+        date: new Date(track.date).toISOString(),
+        startTime: new Date(track.startTime).toISOString(),
+        endTime: new Date(track.endTime).toISOString(),
+        createdAt: new Date(track.createdAt).toISOString(),
+        updatedAt: new Date(track.updatedAt).toISOString(),
+      };
+    },
   },
 });
--- functions/graphql/typedefs.js
+++ functions/graphql/typedefs.js
@@ -1,12 +1,15 @@
 const { gql } = require('apollo-server-lambda');
 
 module.exports = gql`
+  scalar JSON
+  
   type Query {
     allTracks: [Track]
+    track(_id: ID!): Track
   }
 
   type Track {
-    id: ID!
+    _id: ID!
     name: String!
     date: String
     foreignKey: String
@@ -38,6 +41,24 @@ module.exports = gql`
     visualization: String
     startTime: String
     endTime: String
+    geoJson: FeatureCollection
+  }
+
+  type FeatureCollection {
+    type: String
+    features: [Feature]
+  }
+
+  type Feature {
+    type: String
+    properties: JSON 
+    bbox: JSON
+    geometry: Geometry
+  }
+
+  type Geometry {
+    type: String
+    coordinates: JSON
   }
 
   type Point {
